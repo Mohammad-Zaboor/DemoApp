60a351693451fe31abac7f3da0c64298018697ae
Mohammad-Zaboor <41190514+Mohammad-Zaboor@users.noreply.github.com>
2018-07-14 16:02:58 +0530

    Add files via upload
    new

---
+++ src/main/resources/static/js/updatejavascript.js
@@ -0,0 +1,83 @@
+// change contact details
+$(document).ready(function() {
+
+$("#bttn45").click(function() {
+
+    $("#frm").animate( {
+        height:'toggle'
+    });
+   
+});
+
+
+});
+
+// change email
+$(document).ready(function() {
+
+    $("#bttn46").click(function() {
+    
+        $("#frm22").animate( {
+            height:'toggle'
+        });
+       
+    });
+    
+    
+});
+
+// change of password
+$(document).ready(function() {
+
+    $("#bttn47").click(function() {
+    
+        $("#frm33").animate( {
+            height:'toggle'
+        });
+       
+    });
+    
+    
+    });
+
+
+   // delete account
+$(document).ready(function() {
+
+    $("#bttn48").click(function() {
+    
+        $("#frm43").animate( {
+            height:'toggle'
+        });
+       
+    });
+    
+    
+    });
+
+
+    //it will work fo a tag
+    // $(document).ready(function() {
+    //     $("a[href='#top']").click(function() {
+    //         $("html, body").animate({ scrollTop: 0 }, "slow");
+    //         return false;
+    //     });
+    
+    //     $(window).scroll(function(){
+    //          if ($(this).scrollTop() > 200) {
+    //              $('.btn-scroll-to-top').fadeIn();
+    //          } else {
+    //              $('.btn-scroll-to-top').fadeOut();
+    //          }
+    //     });
+    // });
+
+
+  $(function() {
+   $("#Registerfrmzab").click(function()
+    {
+       $("#rgsfrmzab").animate( {
+          height:'toggle'
+       });
+    });
+  });
